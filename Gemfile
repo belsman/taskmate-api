e4ac31b2e1e74e3a6b3b4bf81ea2b1cce93baa6c
Bello Babakolo <fleetbeekay@gmail.com>
2025-05-06 15:07:07 +0100

    Add auth controller

--- Gemfile
+++ Gemfile
@@ -10,8 +10,7 @@ gem "puma", ">= 5.0"
 # gem "jbuilder"
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
-# gem "bcrypt", "~> 3.1.7"
-gem 'bcrypt'
+gem "bcrypt", "~> 3.1.7"
 gem 'jwt'
 gem 'rack-cors'
 
---
+++ app/controllers/api/v1/todos_controller.rb
@@ -0,0 +1,53 @@
+class Api::V1::TodosController < ApplicationController
+    before_action :authorize_request
+    before_action :set_todo, only: [:update, :destroy]
+
+    def index
+      @todos = @current_user.todos
+      render json: @todos
+    end
+
+    def create
+      @todo = @current_user.todos.build(todo_params)
+      if @todo.save
+        render json: @todo, status: :created
+      else
+        render json: { errors: @todo.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      if @todo.update(todo_params)
+        render json: @todo
+      else
+        render json: { errors: @todo.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @todo.destroy
+      head :no_content
+    end
+
+    private
+
+    def set_todo
+      @todo = @current_user.todos.find_by(id: params[:id])
+      return render json: { error: 'Not Found' }, status: :not_found unless @todo
+    end
+
+    def todo_params
+      params.require(:todo).permit(:title, :completed)
+    end
+
+    def authorize_request
+        header = request.headers['Authorization']
+        token = header.split(' ').last if header
+        begin
+          decoded = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+          @current_user = User.find(decoded['user_id'])
+        rescue
+          render json: { errors: 'Unauthorized' }, status: :unauthorized
+        end
+      end
+end
